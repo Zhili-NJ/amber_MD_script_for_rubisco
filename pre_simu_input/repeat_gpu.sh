942ca7dbedac3374cc3f886d8230089503d6530c
Zhili Chen <879909926@qq.com>
2022-04-17 17:51:16 +0100

    scripts of submit md production runs in gpu

---
+++ pre_simu_input/repeat_gpu.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#usage: chmod 755 repeat_gpu.sh
+#nohup ./repeat_gpu.sh 1 &
+#
+i=$1
+
+if [ $i == 1 ]
+then
+   inputrst=equi_1.ncrst
+else 
+   inputrst=$((i-1)).rst7
+fi
+
+if [ $i -le 20 ]
+then
+
+   cat << EOF > production$i.sh
+export CUDA_VISIBLE_DEVICES="0"
+RUN=$i
+
+/usr/local/amber/bin/pmemd.cuda_SPFP -O -i production.in -o $i.out -c $inputrst -p *_HMR.parm7 -ref $inputrst -r $i.rst7 -x $i.nc -inf $i.inf
+
+./repeat_gpu.sh $((i+1)) 
+
+EOF
+
+chmod 755 production$i.sh
+./production$i.sh
+
+fi 
+
