044925857e9daf160537b9847dc2c7a2def18a47
Zhili-NJ <58370340+Zhili-NJ@users.noreply.github.com>
2022-04-20 10:47:20 +0100

    loops of 20 production runs in gpu machines

--- pre_simu_input/repeat_gpu.sh
+++ pre_simu_input/repeat_gpu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #use directory name to distinguish the running systems
-#usage: chmod 755 repeat_gpu.sh
 #CUDA_VISIBLE_DEVICES="0" need to be specifie
+#usage: chmod 755 repeat_gpu.sh
 #nohup ./repeat_gpu.sh 1 &
 
 i=$1
